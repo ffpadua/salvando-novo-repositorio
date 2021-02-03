b6e5150415eec5178daea54590490849b26c6c8d
Filipe Padua <ffpadua97@icloud.com>
2021-02-02 22:46:18 -0300

    Ultimo teste

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,7 +3,8 @@ package application;
 public class Program {
 
     public static void main(String[] args) {
-        System.out.println("Primeiros testes com github");
+        System.out.println("Primeiros testes com github!");
+        System.out.println("Segundo teste!");
     }
 
 }
